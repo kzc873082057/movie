67cf3bc8a254defeb7fefa18d043b580b1d9d878
Administrator <2817902026@qq.com>
2018-06-23 13:48:05 +0800

    前台界面搭建完成

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,7 +1,15 @@
 # _*_coding:utf-8_*
 from . import admin
+from flask import render_template,redirect,url_for
 
-
-@admin.route("/")
+@admin.route('/')
 def index():
-    return "admin page"
+    return " this is andmin"
+
+@admin.route('/login/')
+def login():
+    return render_template('admin/login.html')
+
+@admin.route('/logout/')
+def logout():
+    return redirect(url_for('admin.login'))
